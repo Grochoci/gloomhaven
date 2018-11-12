b7249fdfa3f865334096784c095d4ad5b1225d77
Paul Grochocinski <p.grochocinski@hotmail.ca>
2018-11-11 22:16:28 -0500

    Restructures monster app to have monster container instead of everything instead index

--- app/javascript/apps/monsters/index.vue
+++ app/javascript/apps/monsters/index.vue
@@ -1,31 +1,19 @@
 <template>
-  <div id="monsters-index-vue">
+  <section id="monsters-index-vue">
     <div v-for="monster in monsters">
-      <p> Name: {{monster.name}} </p>
-      <p> Boss: {{monster.is_boss}} </p>
-      <p> Portrait: <img v-bind:src="monster.portrait" class='monster-portrait' /> </p>
-      <p> Stats:
-        <div v-for="stats in monster.monster_stats"
-          :class="{
-            'normal-stats': !stats.is_elite,
-            'elite-stats': stats.is_elite,
-          }">
-            <p> Level: {{stats.level}} </p>
-            <p> Health: {{stats.health}} </p>
-            <p> Attack: {{stats.attack}} </p>
-            <p> Movement: {{stats.movement}} </p>
-            <p> Range: {{stats.range}} </p>
-        </div>
-      </p>
-      <p> Ability Deck: <img v-bind:src="monster.ability_deck.back_image" class='ability-deck-back' /> </p>
+      <monster :monster='monster'></monster>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 import graphqlAjaxCaller from '../../helpers/graphql-ajax-caller';
+import Monster from '../../containers/monsters/monster.vue';
 
 export default {
+  components: {
+    Monster,
+  },
   data() {
     return {
       monsters: [],
@@ -76,16 +64,7 @@ export default {
 </script>
 
 <style scoped>
-  .monster-portrait {
-    height:100px;
-  }
-  .ability-deck-back {
-    height: 100px;
-  }
-  .normal-stats {
-    background-color: lightgrey;
-  }
-  .elite-stats {
-    background-color: gold;
+  #monsters-index-vue {
+    text-align: center;
   }
 </style>
---
+++ app/javascript/containers/monsters/monster.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+      <img v-bind:src="monster.portrait" class='monster-portrait' />
+      <p class='monster-name'> {{monster.name}} </p>
+      <p>
+        <div v-for="stats in monster.monster_stats"
+          :class="{
+            'normal-stats': !stats.is_elite,
+            'elite-stats': stats.is_elite,
+          }">
+            <p> Level: {{ stats.level }} </p>
+            <p> Health: {{ stats.health }} </p>
+            <p> Attack: {{ stats.attack }} </p>
+            <p> Movement: {{ stats.movement }} </p>
+            <p> Range: {{ stats.range || '-' }} </p>
+        </div>
+      </p>
+      <img v-bind:src="monster.ability_deck.back_image" class='ability-deck-back' />
+    </div>
+</template>
+
+<script>
+import graphqlAjaxCaller from '../../helpers/graphql-ajax-caller';
+
+export default {
+  data() {
+    return {
+    };
+  },
+  props: {
+    monster: Object,
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+  .monster-portrait {
+    height:200px;
+  }
+  .ability-deck-back {
+    height: 100px;
+  }
+  .normal-stats {
+    background-color: lightgrey;
+  }
+  .elite-stats {
+    background-color: gold;
+  }
+  .monster-name {
+    font-size: 20px;
+    font-weight: 800;
+  }
+</style>
