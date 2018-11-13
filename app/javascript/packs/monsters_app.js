4ec9c0513f613f6b7478e2697371e5213b0a9731
Paul Grochocinski <p.grochocinski@hotmail.ca>
2018-11-13 00:35:49 -0500

    Adds bootstrap vue and its css to the monsters app

--- app/javascript/packs/monsters_app.js
+++ app/javascript/packs/monsters_app.js
@@ -1,5 +1,11 @@
 import Vue from 'vue';
 import App from '../apps/monsters/index.vue';
+import BootstrapVue from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
