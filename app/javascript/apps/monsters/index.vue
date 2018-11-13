8675ee216fcaad88ce4f978af866cfebe7a45740
Paul Grochocinski <p.grochocinski@hotmail.ca>
2018-11-13 01:27:40 -0500

    Adds monster list with button to trigger div. Also adds possibility to click monster and replace selectedMonster. Has some lines to be removed once real data is populated

--- app/javascript/apps/monsters/index.vue
+++ app/javascript/apps/monsters/index.vue
@@ -1,7 +1,29 @@
 <template>
   <section id="monsters-index-vue">
-    <div v-for="monster in monsters">
-      <monster :monster='monster'></monster>
+    <div v-show="!showMonsterList" class="monster-content">
+      <monster v-if="selectedMonster"
+               :monster='selectedMonster'>
+      </monster>
+    </div>
+    <div v-show="showMonsterList" class="monster-list">
+      <b-card-group v-for="rowNumber in rowCount">
+        <b-card v-for="monster in monsterList.slice((rowNumber - 1) * itemsPerRow, rowNumber * itemsPerRow)"
+                :title="monster.name"
+                :img-src="monster.portrait"
+                img-fluid
+                :img-alt="monster.name"
+                img-top
+                @click="selectMonster(monster)"
+                class="monster-card">
+        </b-card>
+      </b-card-group>
+    </div>
+    <div class="monster-footer">
+      <b-button :pressed.sync="showMonsterList"
+                :variant="showMonsterListButton.variant"
+                class="monster-show-button">
+        {{ showMonsterListButton.text }}
+      </b-button>
     </div>
   </section>
 </template>
@@ -17,22 +39,64 @@ export default {
   data() {
     return {
       monsters: [],
+      monsterList: [],
+      selectedMonster: null,
+      showMonsterList: false,
+      itemsPerRow: 5,
+
     };
   },
-  beforeMount() {
-    this.getMonsters();
+  computed: {
+    showMonsterListButton() {
+      const text = !this.showMonsterList ? "Show All Monsters" : "Hide All Monsters";
+      const variant = !this.showMonsterList ? "primary" : "danger";
+
+      return {
+        variant: variant,
+        text: text,
+      }
+    },
+
+    rowCount() {
+      return Math.ceil(this.monsterList.length / this.itemsPerRow);
+    },
+  },
+  mounted() {
+    this.getMonsterList();
   },
   methods: {
-    getMonsters() {
+    getMonsterList() {
       const self = this;
 
       const successCallback = (res) => {
-        self.monsters = res.data.allMonsters;
+        self.monsterList = res.data.allMonsters;
+        // TODO: Remove once lots of monsters are added
+        self.monsterList = self.monsterList.concat(self.monsterList);
+        self.monsterList = self.monsterList.concat(self.monsterList);
+        // TODO: Remove once selection is possible
+        self.selectedMonster = self.monsterList[0];
+
+        // Make Empty objects to fill a row when displaying
+        const emptyMonsterCount = 5 - self.monsterList.length % 5;
+        for(let i=0; i < emptyMonsterCount; i++){
+          self.monsterList = self.monsterList.concat([
+            {id: null, name: "", portrait: "", is_boss: false}
+          ])
+        }
       };
 
       const errorCallback = (res) => {
         console.error(res);
       };
+
+      // const query = `{
+      //   allMonsters {
+      //     id,
+      //     name,
+      //     portrait,
+      //     is_boss,
+      //   }
+      // }`;
 
       const query = `{
         allMonsters {
@@ -59,6 +123,10 @@ export default {
 
       graphqlAjaxCaller(query, successCallback, errorCallback);
     },
+    selectMonster(monster) {
+      this.selectedMonster = monster;
+      this.showMonsterList = false;
+    },
   },
 };
 </script>
@@ -66,5 +134,23 @@ export default {
 <style scoped>
   #monsters-index-vue {
     text-align: center;
+  }
+  .monster-content {
+    padding-bottom:40px;   /* Height of the footer */
+  }
+  .monster-list {
+  }
+  .monster-footer {
+    position:absolute;
+    bottom:0;
+    width:100%;
+    height:40px;   /* Height of the footer */
+  }
+  .monster-show-button {
+    width: 100%;
+    height:40px;
+  }
+  .monster-card {
+    cursor: pointer;
   }
 </style>
